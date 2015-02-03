2098ff03b673f3779e1e9ba94bc1b6e55ee8d338
Ho-Wei Kang <howeik@gmail.com>
2015-02-02 17:11:53 -0800

    prints ip of clients

--- httpd.cpp
+++ httpd.cpp
@@ -333,6 +333,7 @@ int main (int argc, char* argv[]) {
         // call accept() to get a new socket for each client connection
         int csock = accept(sock, (struct sockaddr*) &client_address, &ca_len);
         cout << "Connected to: " << client_address.sin_addr.s_addr << endl;
+        cout << "Connection from: " << inet_ntoa(client_address.sin_addr) << endl;
 
         if (csock < 0) {
             perror("accept failed");

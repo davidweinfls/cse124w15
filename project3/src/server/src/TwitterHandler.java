68439383d2165a47aa49e9d6b9abcac85e9fae44
David <davidweinfls@163.com>
2015-03-09 16:22:01 -0700

    implemented posting tweets

--- project3/src/server/src/TwitterHandler.java
+++ project3/src/server/src/TwitterHandler.java
@@ -11,14 +11,16 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterHandler implements Twitter.Iface {
     protected Map<String, List <String>> user_subs;
-    protected Map<String, List <Long>> user_tweets;
+    protected Map<String, List <Tweet>> user_tweets;
+    protected int id = 0;
 
     public TwitterHandler(Map<String, List<String>> map) {
         this.user_subs = map;
-        this.user_tweets = new HashMap<String, List<Long>> ();
+        this.user_tweets = new HashMap<String, List<Tweet>> ();
     }
 
     @Override
@@ -35,7 +37,7 @@ public class TwitterHandler implements Twitter.Iface {
             throw new AlreadyExistsException();
         } else {
             user_subs.put(handle, new ArrayList<String>());
-            user_tweets.put(handle, new ArrayList<Long>());
+            user_tweets.put(handle, new ArrayList<Tweet>());
             System.out.println("created user: " + handle);
         }
     }
@@ -44,18 +46,69 @@ public class TwitterHandler implements Twitter.Iface {
     public void subscribe(String handle, String theirhandle)
         throws NoSuchUserException
     {
+        if (user_subs.containsKey(handle)) {
+            if (user_subs.containsKey(theirhandle)) {
+                if (user_subs.get(handle).contains(theirhandle)) {
+                    System.err.println(handle + " already subscribed " + theirhandle);
+                } else {
+                    user_subs.get(handle).add(theirhandle);
+                }
+            } else {
+                System.err.println("error - trying to subscribe a non-existent user: " + theirhandle);
+                throw new NoSuchUserException();
+            }
+        } else {
+            System.err.println("user: " + handle + " does not exist");
+            throw new NoSuchUserException();
+        }
     }
 
     @Override
     public void unsubscribe(String handle, String theirhandle)
         throws NoSuchUserException
     {
+        if (user_subs.containsKey(handle)) {
+            if (user_subs.containsKey(theirhandle)) {
+                if (!user_subs.get(handle).contains(theirhandle)) {
+                    System.err.println(handle + " already unsubscribed " + theirhandle);
+                } else {
+                    user_subs.get(handle).remove(theirhandle);
+                }
+            } else {
+                System.err.println("error - trying to unsubscribe a non-existent user: " + theirhandle);
+                throw new NoSuchUserException();
+            }
+        } else {
+            System.err.println("user: " + handle + " does not exist");
+            throw new NoSuchUserException();
+        }
+    }
+
+    public long getTimeInSeconds() {
+        long ms = System.currentTimeMillis();
+        long s = TimeUnit.MILLISECONDS.toSeconds(ms);
+        return s;
     }
 
     @Override
     public void post(String handle, String tweetString)
         throws NoSuchUserException, TweetTooLongException
     {
+        if (user_subs.containsKey(handle)) {
+            if (tweetString.length() > 140) {
+                System.err.println("tweet exceed maximum length");
+                throw new TweetTooLongException();
+            }
+            // create a new tweet
+            id++;
+            Tweet t = new Tweet(id, handle, getTimeInSeconds(), 0, tweetString);
+            System.out.println("here");
+            user_tweets.get(handle).add(t);
+            System.out.println("user: " + handle + " posted a tweet, id: " + id);
+        } else {
+            System.err.println("user: " + handle + " does not exist");
+            throw new NoSuchUserException();
+        }
     }
 
     @Override

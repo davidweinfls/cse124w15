4ddf863ad14ed866ea082be7b0a3e35a4cc47da4
David <davidweinfls@163.com>
2015-03-11 17:59:45 -0700

    add cl.py

--- project3/src/client/cl.py
+++ project3/src/client/cl.py
@@ -30,6 +30,45 @@ try:
   client.ping()
   print 'ping()'
 
+  client.createUser("@user1")
+  print '@user1 created'
+
+  client.createUser("@user2")
+  print '@user2 created'
+
+  client.createUser("@david")
+  print '@david created'
+
+  client.subscribe("@david", "@user1")
+  print 'subscribe user1'
+
+  #client.subscribe("@david", "@user1")
+  #print 'already subscribed user1'
+
+  #client.subscribe("@david", "@user2")
+  #print 'cannot subscribe user2, user2 does not exist'
+
+  client.unsubscribe("@david", "@user1")
+  print 'unsubscribe user1'
+
+  client.unsubscribe("@david", "@user2")
+  print 'nop - unsubscribe user2'
+
+  client.subscribe("@user1", "@user1")
+  print '@user1 subscribe himself'
+
+  client.post("@user1", "first tweet")
+  print '@user1 posted first tweet'
+
+  client.star("@user2", 1)
+  print '@user2 star tweet 1'
+
+  client.star("@user1", 1)
+  print '@user1 star tweet 1'
+
+  client.star("@user2", 1)
+  print '@user2 cannot star tweet 1 again'
+
   # Close!
   transport.close()
 
